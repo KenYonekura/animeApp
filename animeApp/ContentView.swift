54a944d25421fbf1c2c68846d0cc77f74615b8f4
KenYonekura <p.p.high.llows@gmail.com>
2020-09-07 18:49:47 +0900

    Initial Commit

---
+++ animeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  animeApp
+//
+//  Created by 米倉謙 on 2020/09/07.
+//  Copyright © 2020 kenyonekura.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
